65850045d55932cc0a179f76351a08a8223ecb39
Shuchir Jain <53063247+ShuchirJ@users.noreply.github.com>
2026-09-22 16:30:23 -0400

    Add run.sh to manage Tailscale service

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -uo pipefail
+ 
+cd "${HOME}"
+ 
+while true; do
+  ./tailscaled --tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055
+  sleep 1
+done &
+ 
+while true; do
+  ./tailscale up --hostname=vpn-panda --advertise-exit-node
+  sleep 1
+done &
+ 
+sleep infinity
+ 
